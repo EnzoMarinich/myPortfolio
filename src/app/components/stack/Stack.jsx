57d43405218fed417b817024d8136d7d291e1b01
EnzoMarinich <marinichenzo@gmail.com>
2023-10-11 05:26:16 -0300

    lorem

--- src/app/components/stack/Stack.jsx
+++ src/app/components/stack/Stack.jsx
@@ -18,47 +18,77 @@ import sass from "../../../../public/sass.svg";
 import { themeDarkContext } from "@/app/useContext/theme";
 import Image from "next/image";
 
-
-
-
-
-
 export const Stack = () => {
-
-  const {themeDark} = useContext(themeDarkContext)
+  const { themeDark } = useContext(themeDarkContext);
 
   const stack = [
     { icon: html5.src, name: "HTML", link: "https://lenguajehtml.com/html/" },
-    { icon: css.src, name: "CSS", link: "https://lenguajecss.com/css/introduccion/que-es-css/" },
-    { icon: js.src, name: "Javascript", link: "https://lenguajejs.com/javascript/" },
+    {
+      icon: css.src,
+      name: "CSS",
+      link: "https://lenguajecss.com/css/introduccion/que-es-css/",
+    },
+    {
+      icon: js.src,
+      name: "Javascript",
+      link: "https://lenguajejs.com/javascript/",
+    },
     { icon: next.src, name: "Next.js", link: "https://nextjs.org/" },
     { icon: react.src, name: "React", link: "https://es.react.dev/" },
     { icon: redux.src, name: "Redux", link: "https://redux.js.org/" },
-    { icon: boostrap.src, name: "Bootstrap", link: "https://getbootstrap.com/" },
-    { icon: tailwind.src, name: "Tailwind CSS", link: "https://tailwindui.com/" },
+    {
+      icon: boostrap.src,
+      name: "Bootstrap",
+      link: "https://getbootstrap.com/",
+    },
+    {
+      icon: tailwind.src,
+      name: "Tailwind CSS",
+      link: "https://tailwindui.com/",
+    },
     { icon: sass.src, name: "Sass", link: "https://sass-lang.com/" },
     { icon: node.src, name: "Node.js", link: "linhttps://nodejs.org/esk" },
     { icon: mySql.src, name: "MySQL", link: "https://www.mysql.com/" },
-    { icon: postgre.src, name: "PostgreSQL", link: "https://www.postgresql.org/" },
+    {
+      icon: postgre.src,
+      name: "PostgreSQL",
+      link: "https://www.postgresql.org/",
+    },
     { icon: sequelize.src, name: "Sequelize", link: "https://sequelize.org/" },
     { icon: git.src, name: "GIT", link: "https://git-scm.com/" },
-    { icon: firebase.src, name: "Firebase", link: "https://firebase.google.com/" },
+    {
+      icon: firebase.src,
+      name: "Firebase",
+      link: "https://firebase.google.com/",
+    },
   ];
   return (
     <section id="stack" className={style.container}>
       <div className={style.title}>
-        <h1 style={themeDark? {} : {color:"rgb(14, 149, 169)"}}>Technology Stack</h1>
-        <span>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates,
-          repudiandae.
-        </span>
+        <h1 style={themeDark ? {} : { color: "rgb(14, 149, 169)" }}>
+          Technology Stack
+        </h1>
+        <span>Technologies that I have used and learned in my career</span>
       </div>
       <div className={style.cardContainer}>
         {stack.map((stack) => {
           return (
-            <a target="_blank" href={stack.link} key={stack.name} className={` ${style.card} ${themeDark? style.dark : style.clear}`}>
+            <a
+              target="_blank"
+              href={stack.link}
+              key={stack.name}
+              className={` ${style.card} ${
+                themeDark ? style.dark : style.clear
+              }`}
+            >
               <div className={style.cardDiv}>
-                <Image className={style.cardImg} src={stack.icon} width={500} height={500} alt="" />
+                <Image
+                  className={style.cardImg}
+                  src={stack.icon}
+                  width={500}
+                  height={500}
+                  alt=""
+                />
                 <span>{stack.name}</span>
               </div>
             </a>
@@ -68,5 +98,3 @@ export const Stack = () => {
     </section>
   );
 };
-
-
