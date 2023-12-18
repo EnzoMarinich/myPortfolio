de96ea146f1fb5403f98919379a13fd1ac567531
EnzoMarinich <marinichenzo@gmail.com>
2023-12-18 12:16:32 -0300

    style: :art: profile picture change

--- src/app/components/landing/Landing.jsx
+++ src/app/components/landing/Landing.jsx
@@ -1,36 +1,33 @@
-"use client"
+"use client";
 import React, { useContext, useEffect } from "react";
 import style from "./Landing.module.css";
 import Image from "next/image";
-import foto from "../../../../public/fotoPerfil-sinfondo-preview.png";
+import foto from "../../../../public/foto.jpg";
 import { Links } from "../Links";
 import { themeDarkContext } from "@/app/useContext/theme";
 
 export const Landing = () => {
-  const {themeDark} = useContext(themeDarkContext)
+  const { themeDark } = useContext(themeDarkContext);
 
   const texts = [
     "Full Stack Developer",
     "Front End Developer",
-    "Back End Developer"
+    "Back End Developer",
   ];
-  
-  
+
   let index = 0;
   let charIndex = 0;
   let isDeleting = false;
-  
 
   const changeText = () => {
     const currentText = texts[index];
     const container = document.querySelector("#textChanging");
     const currentTextLength = currentText.length;
 
-  
     if (isDeleting) {
       container.textContent = currentText.substring(0, charIndex - 1);
       charIndex--;
-  
+
       if (charIndex === 0) {
         isDeleting = false;
         index = (index + 1) % texts.length;
@@ -38,42 +35,59 @@ export const Landing = () => {
     } else {
       container.textContent = currentText.substring(0, charIndex + 1);
       charIndex++;
-  
+
       if (charIndex === currentTextLength) {
         isDeleting = true;
       }
     }
-  
+
     const speed = isDeleting ? 50 : 150; // Velocidad de escritura y borrado
     setTimeout(changeText, speed);
   };
-  
+
   useEffect(() => {
     changeText();
   }, []);
 
-
   return (
     <section id="landing" className={style.container}>
       <div className={style.title}>
-        <span style={{marginTop: "10px"}}>
-          Chaco | Argentina
-        </span>
+        <span style={{ marginTop: "10px" }}>Chaco | Argentina</span>
       </div>
       <div className={style.perfil}>
         <div className={style.desc}>
-          <h1 style={themeDark? {} : {color: "black"}}>Enzo Marinich</h1>
-          <div style={{display: "flex", marginTop: "-25px"}}>
-            <span className={style.guion} style={themeDark? {color: "rgb(14 14 16)"} : {color: "white"}}>-</span>
-            <span className={style.changingText} style={themeDark? {color: "rgb(113 113 122)"} : {color: "rgb(0 222 255)"}} id="textChanging"></span>
-            <span className={style.cursor} id="cursor">|</span>
+          <h1 style={themeDark ? {} : { color: "black" }}>Enzo Marinich</h1>
+          <div style={{ display: "flex", marginTop: "-25px" }}>
+            <span
+              className={style.guion}
+              style={
+                themeDark ? { color: "rgb(14 14 16)" } : { color: "white" }
+              }
+            >
+              -
+            </span>
+            <span
+              className={style.changingText}
+              style={
+                themeDark
+                  ? { color: "rgb(113 113 122)" }
+                  : { color: "rgb(0 222 255)" }
+              }
+              id="textChanging"
+            ></span>
+            <span className={style.cursor} id="cursor">
+              |
+            </span>
           </div>
-          <div className={style.links} style={themeDark? {} : {color: "black"}}>
-            <Links/>
+          <div
+            className={style.links}
+            style={themeDark ? {} : { color: "black" }}
+          >
+            <Links />
           </div>
         </div>
         <div className={style.image}>
-          <Image src={foto} alt="enzo" />
+          <Image src={foto} width={400} className="rounded-full" alt="enzo" />
         </div>
       </div>
     </section>
